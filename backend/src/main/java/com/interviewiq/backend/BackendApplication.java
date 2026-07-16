b2c4841461616918d1508b9569e08a43562e4b14
Prince2206852-pixel <kumarprince2206852@gmail.com>
2026-07-16 13:51:12 +0530

    Day 3: Domain model, JPA entities and repositories completed

--- backend/src/main/java/com/interviewiq/backend/BackendApplication.java
+++ backend/src/main/java/com/interviewiq/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.interviewiq.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackendApplication {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
         SpringApplication.run(BackendApplication.class, args);
     }
 
